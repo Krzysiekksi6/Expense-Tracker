dc16eab25f951d70ad8a5e5de7c9072e06c0c3bf
Krzysiek <krzysiekksi6@gmail.com>
2023-04-04 20:44:39 +0200

    bottom nav

--- App.tsx
+++ App.tsx
@@ -1,35 +1,49 @@
 /* eslint-disable */
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
-  import AllExpenses from './screens/AllExpenses';
-
+import {Text, StyleSheet, StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import ManageExpenses from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
 
 function App(): JSX.Element {
- 
+  const Stack = createStackNavigator();
+  const BottomTabs = createBottomTabNavigator();
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const ExpensesOverview = () => {
+    return (
+      <BottomTabs.Navigator>
+        <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+        <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+      </BottomTabs.Navigator>
+    );
+  };
   return (
-    <SafeAreaView>
-      <AllExpenses/>
-    </SafeAreaView>
+    <>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Text style={styles.highlight}>Apps screen!</Text>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ExpensesOverview" component={ExpensesOverview} />
+          <Stack.Screen name="ManageExpense" component={ManageExpenses} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
   highlight: {
     fontWeight: '700',
   },
--- screens/AllExpenses.tsx
+++ screens/AllExpenses.tsx
@@ -1,18 +1,9 @@
 /* eslint-disable */
 import React from 'react';
-import {
-  Text,
-  StyleSheet,
-} from 'react-native';
-
+import {Text, StyleSheet} from 'react-native';
 
 function AllExpenses(): JSX.Element {
- 
-  return (
-    <Text style={styles.highlight}>
-        AllExpenses screen!
-    </Text>
-  );
+  return <Text style={styles.highlight}>AllExpenses screen!</Text>;
 }
 
 const styles = StyleSheet.create({
