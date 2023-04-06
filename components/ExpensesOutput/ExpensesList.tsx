6699f0449a635001bbeee86508163791b80f0ff7
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-04-06 12:51:33 +0200

    add Expenses List

--- components/ExpensesOutput/ExpensesList.tsx
+++ components/ExpensesOutput/ExpensesList.tsx
@@ -2,6 +2,21 @@
 import React from 'react';
 import {View, StyleSheet, Text, FlatList} from 'react-native';
 
+/* eslint-disable */
+import React from 'react';
+import {View, StyleSheet, Text, FlatList} from 'react-native';
+
+const ExpensesList = ({expenses}) => {
+  const renderExpenseItem = (itemData) => {
+    return (
+      <Text>{itemData.item.description}</Text>
+    );
+  }
+  return <FlatList data={expenses} renderItem={renderExpenseItem} keyExtractor={(item) => item.id}/>;
+};
+
+export default ExpensesList;
+
 
 
 export default ExpensesList;
