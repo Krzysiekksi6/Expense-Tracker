c180260c09c14a46324e552b5c593ca401aaaa52
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-04-06 12:53:11 +0200

    add Expenses Output

--- components/ExpensesOutput/ExpensesOutput.tsx
+++ components/ExpensesOutput/ExpensesOutput.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable */
+
 import React from 'react';
-import PropTypes from 'prop-types';
 import {View, StyleSheet, Text, FlatList} from 'react-native';
-import ExpensesSummary from './ExpensesSummary';
-import ExpensesList from './ExpensesList';
 
+const ExpensesSummary = ({expenses, peroidName}) => {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+  return (
+    <View>
+      <Text>{peroidName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
+    </View>
+  );
+};
 
-
-
-export default ExpensesOutput;
+export default ExpensesSummary;
