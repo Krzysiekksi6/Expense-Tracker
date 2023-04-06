db0cdb052a15dab01f337b14bf9bfe805e7eff3d
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-04-06 12:54:54 +0200

    add AllExpenses

--- screens/AllExpenses.tsx
+++ screens/AllExpenses.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import {Text, StyleSheet} from 'react-native';
 import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
 
+function AllExpenses(): JSX.Element {
+  return <ExpensesOutput expensesPeroid="Total"/>;
+}
 
 
 
