5531a2b1651e503f157d44d217db713d83e6aa11
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-04-06 12:54:08 +0200

    add Expneses Sumarry

--- components/ExpensesOutput/ExpensesOutput.tsx
+++ components/ExpensesOutput/ExpensesOutput.tsx
@@ -1,18 +1,61 @@
 /* eslint-disable */
-
 import React from 'react';
+import PropTypes from 'prop-types';
 import {View, StyleSheet, Text, FlatList} from 'react-native';
+import ExpensesSummary from './ExpensesSummary';
+import ExpensesList from './ExpensesList';
 
-const ExpensesSummary = ({expenses, peroidName}) => {
-  const expensesSum = expenses.reduce((sum, expense) => {
-    return sum + expense.amount;
-  }, 0);
+const EXPNESES = [
+  {
+    id: 'e1',
+    descriptions: 'Avocado',
+    amount: 3.99,
+    date: new Date('2023-03-12'),
+  },
+
+  {
+    id: 'e2',
+    descriptions: 'T-shirt',
+    amount: 16.99,
+    date: new Date('2023-02-11'),
+  },
+
+  {
+    id: 'e3',
+    descriptions: 'Some bananas',
+    amount: 24.99,
+    date: new Date('2023-01-01'),
+  },
+
+  {
+    id: 'e4',
+    descriptions: 'Programing in Java Book',
+    amount: 71.69,
+    date: new Date('2022-12-12'),
+  },
+
+  {
+    id: 'e5',
+    descriptions: 'Audi A8',
+    amount: 60000,
+    date: new Date('2021-09-27'),
+  },
+];
+
+function ExpensesOutput({expensesPeroid}): JSX.Element {
+ 
   return (
     <View>
-      <Text>{peroidName}</Text>
-      <Text>${expensesSum.toFixed(2)}</Text>
+      <ExpensesSummary expenses={EXPNESES} peroidName={expensesPeroid} />
+      <ExpensesList expenses={EXPNESES}/>
     </View>
   );
-};
+}
 
-export default ExpensesSummary;
+const styles = StyleSheet.create({
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default ExpensesOutput;
--- components/ExpensesOutput/ExpensesSummary.tsx
+++ components/ExpensesOutput/ExpensesSummary.tsx
@@ -3,6 +3,16 @@
 import React from 'react';
 import {View, StyleSheet, Text, FlatList} from 'react-native';
 
-
+const ExpensesSummary = ({expenses, peroidName}) => {
+  const expensesSum = expenses.reduce((sum, expense) => {
+    return sum + expense.amount;
+  }, 0);
+  return (
+    <View>
+      <Text>{peroidName}</Text>
+      <Text>${expensesSum.toFixed(2)}</Text>
+    </View>
+  );
+};
 
 export default ExpensesSummary;
