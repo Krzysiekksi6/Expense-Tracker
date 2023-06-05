1b4640ad5b1ec1940c22adac6587277c7fe7dcab
Daniel Sikora <danielsikora100@gmail.com>
2023-06-05 17:54:26 +0200

    Add screen

--- screens/SignUpScreen.tsx
+++ screens/SignUpScreen.tsx
@@ -1,64 +1,25 @@
-/* eslint-disable */
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, Image} from 'react-native';
-import CustomInput from '../UI/AuthInput/CustomInput';
-import CustomButton from '../UI/AuthButton/CustomButton';
-
+import React, {useContext} from 'react';
+import {createUser, login} from '../util/auth';
+import {Alert} from 'react-native';
+import {AuthContext} from '../store/auth-context';
+import AuthContent from '../components/Auth/AuthContent';
 
 const SignUpScreen = () => {
-  const [email, setEmail] = useState('');
+  const authContext = useContext(AuthContext);
 
-  const onSignInPressed = () => {};
-  const onSignUpPressed = () => {};
+  async function loginHandler(email, password) {
+    try {
+      const token = await createUser(email, password);
+      authContext.authenticate(token);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        'Authentication failed!',
+        'Could not register you. Please check your credentials or try again later!',
+      );
+    }
+  }
 
-  return (
-    <View style={styles.container}>
-    
-      <View style={styles.label}>
-        <Text style={styles.labelText}>Sign up</Text>
-      </View>
-      <CustomInput value={email} setValue={setEmail} placeholder={'email'} />
-      <CustomInput
-        value={email}
-        setValue={setEmail}
-        placeholder={'password'}
-        secureTextEntry={true}
-      />
-      <CustomButton onPress={onSignUpPressed} text={'Sign up'} />
-      <View style={styles.footer}>
-        <Text style={{marginRight: 10}}>Have an Account?</Text>
-        <CustomButton
-          onPress={onSignInPressed}
-          text={'Sign in'}
-          type="TERTIARY"
-          width="33%"
-        />
-      </View>
-    </View>
-  );
+  return <AuthContent isLogin={false} onAuthenticate={loginHandler} />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    padding: 20,
-  },
-
-  label: {
-    marginVertical: 20,
-  },
-  labelText: {
-    fontSize: 24,
-    fontWeight: '600',
-    marginLeft: 5,
-  },
-  footer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    marginVertical: 10,
-  },
-});
-
 export default SignUpScreen;
