7a56b2991e4aa495fc6d77360b558586b78f5e6d
Daniel Sikora <danielsikora100@gmail.com>
2023-06-05 17:46:16 +0200

    Add screen

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,16 +1,29 @@
 /* eslint-disable */
-import React, {useLayoutEffect, useContext} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useLayoutEffect, useContext, useState} from 'react';
+import {View, StyleSheet, TextInput} from 'react-native';
 import IconButton from '../UI/IconButton';
 import {GlobalStyles} from '../constans/styles';
 import {GlobalColors} from '../constans/styles';
 import Button from '../UI/Button';
 import {ExpensesContext} from '../store/expenses-context';
+import ExpenseFrom from '../components/ManageExpense/ExpenseFrom';
+import {Expense} from '../constans/Expense';
+import {storeExpense, updateExpense, deleteExpense} from '../util/http';
+import LoadingOverlay from '../UI/LoadingOverlay';
+import ErrorOverlay from '../UI/ErrorOverlay';
+import {AuthContext} from '../store/auth-context';
 
 function ManageExpenses({route, navigation}): JSX.Element {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
   const expensesCtx = useContext(ExpensesContext);
+  const authCtx = useContext(AuthContext);
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
+
+  const selectedExpense = expensesCtx.expenses.find(
+    expense => expense.id === editedExpenseId,
+  );
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -23,42 +36,59 @@ function ManageExpenses({route, navigation}): JSX.Element {
   const cancelHandler = () => {
     navigation.goBack();
   };
-  const deleteExpenseHandler = () => {
-    expensesCtx.deleteExpense(editedExpenseId);
-    navigation.goBack();
-  };
-  const confirmHandler = () => {
-    if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: 'Test!!!',
-        amount: 7.99,
-        date: new Date('2022-06-06'),
-      });
-    } else {
-      expensesCtx.addExpense({
-        description: 'Test',
-        amount: 7.99,
-        date: new Date('2022-05-05'),
-      });
+  const deleteExpenseHandler = async () => {
+    setIsSubmitting(true);
+    try {
+      await deleteExpense(editedExpenseId, authCtx.token);
+      expensesCtx.deleteExpense(editedExpenseId);
+      navigation.goBack();
+    } catch (error) {
+      setError('Could not delete expense!');
+      setIsSubmitting(false);
     }
-    navigation.goBack();
   };
+  const confirmHandler = async (expenseData: Expense) => {
+    setIsSubmitting(true);
+    try {
+      if (isEditing) {
+        expensesCtx.updateExpense(editedExpenseId, expenseData);
+        await updateExpense(editedExpenseId, expenseData, authCtx.token);
+      } else {
+        const id = await storeExpense(expenseData, authCtx.token);
+        expensesCtx.addExpense({...expenseData, id: id});
+      }
+      navigation.goBack();
+    } catch (error) {
+      setError('Could not confirm expense!');
+      setIsSubmitting(false);
+    }
+  };
+
+  function errorHandler(): void {
+    setError(null);
+  }
+  if (error && !isSubmitting) {
+    return <ErrorOverlay message={error} onConfirm={errorHandler} />;
+  }
+
+  if (isSubmitting) {
+    return <LoadingOverlay />;
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.buttonContainer}>
-        <Button style={styles.button} mode={'flat'} onPress={cancelHandler}>
-          Cancel
-        </Button>
-        <Button style={styles.button} onPress={confirmHandler} mode={null}>
-          {isEditing ? 'Update' : 'Add'}
-        </Button>
-      </View>
+      <ExpenseFrom
+        onSubmit={confirmHandler}
+        onCancel={cancelHandler}
+        submitButtonLabel={isEditing ? 'Update' : 'Add'}
+        defaultValues={selectedExpense}
+      />
+
       {isEditing && (
         <View style={styles.deleteContainer}>
           <Button
             style={styles.deleteButton}
-            mode={'error'}
+            mode={null}
             onPress={deleteExpenseHandler}>
             Delete
           </Button>
@@ -79,15 +109,7 @@ const styles = StyleSheet.create({
     padding: 24,
     backgroundColor: GlobalColors.colors.white50,
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignContent: 'center',
-  },
-  button: {
-    minWidth: 120,
-    marginHorizontal: 8,
-  },
+
   deleteButton: {
     minWidth: 120,
     marginTop: 16,
